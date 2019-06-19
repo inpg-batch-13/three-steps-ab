5af18d734e14db21db97613ea3743eb57be6894e
rahmatrhd <kickyou44@gmail.com>
2019-06-19 21:47:40 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function threeStepsAB (text) {
+// Your code here
+}
+
+// TEST CASES
+console.log(threeStepsAB('lane borrowed')); // true
+console.log(threeStepsAB('i am sick')); // false
+console.log(threeStepsAB('you are boring')); // true
+console.log(threeStepsAB('barbarian')); // false
+console.log(threeStepsAB('bacon and meat')); // false
