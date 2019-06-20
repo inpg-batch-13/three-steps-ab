d39ca510aa7f59d25c1040ec7fa6d04eae777fd9
felishaww <felishamiranda1510@gmail.com>
2019-06-20 18:27:24 +0700

    done

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function threeStepsAB (text) {
-// Your code here
-}
 
-// TEST CASES
-console.log(threeStepsAB('lane borrowed')); // true
-console.log(threeStepsAB('i am sick')); // false
-console.log(threeStepsAB('you are boring')); // true
-console.log(threeStepsAB('barbarian')); // false
-console.log(threeStepsAB('bacon and meat')); // false
+    for (var i = text.length - 1; i > 4; i--) {
+    
+        if (text[i] == 'b'|| text[i] == 'B' && text[i - 4] == 'a' || text[i - 4] == 'A') {
+            return true
+        }
+       
+    }
+    return false
+    }
+    
+    // TEST CASES
+    console.log(threeStepsAB('lane borrowed')); // true
+    console.log(threeStepsAB('i am sick')); // false
+    console.log(threeStepsAB('you are boring')); // true
+    console.log(threeStepsAB('barbarian')); // false
+    console.log(threeStepsAB('bacon and meat')); // false
